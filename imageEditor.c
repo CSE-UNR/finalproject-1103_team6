ef7e0e2c569da41736a9abd2e4dc8b9391cdb8a1
Brody Mingo <brodycoot05@gmail.com>
2024-05-05 23:34:29 +0000

    testing

---
+++ imageEditor.c
@@ -0,0 +1,37 @@
+//Name: Brody Mingo & Natalie Jimenez
+//Date: 5/3/24
+//Purpose: Editing Grayscale Images
+
+int main(){
+	printf("welcome to the Image Editor, please select an option below: \n");
+	printf("1 - Load A New Image \n");
+	printf("2 - Display The Current Image \n");
+	printf("3 - Edit The Current Image \n");
+	printf("option: ");
+	return 0;
+}
+
+loadImage(){
+
+	return ;
+}
+
+displayImage(){
+
+	return ;
+}
+
+crop(){
+
+	return ;
+}
+
+dim(){
+
+	return ;
+}
+
+brighten(){
+
+	return ;
+}
